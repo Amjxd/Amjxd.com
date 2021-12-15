b6b49b44dff4458aea34d9c67b768b3a741c1f69
Amjxd <64230420+Amjxd@users.noreply.github.com>
2021-12-14 21:32:11 -0500

    Update workExperience.php

--- Final Project Heroku Upload/workExperience.php
+++ Final Project Heroku Upload/workExperience.php
@@ -15,16 +15,15 @@
 
 
 <!-- Linking Custom CSS File -->
-<link rel="stylesheet" href="workExperience.css">
-
+<link rel="stylesheet" href="education.css">
 
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+<link href = "https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto+Slab|Roboto:300,400,500,700" rel = "stylesheet" />
 
 
   </head>
   <body>
-    
     
     <!-- ####################### START HEADER AREA ########################## -->
     <div class="header_area">
@@ -49,70 +48,111 @@
     </div>
     <!-- ####################### END HEADER AREA ########################## -->
     
-
-  <!-- ####################### START WORK EXPERIENCE JUMBOTRON ########################## -->
+    
+    
+      <!-- ####################### START EDUCATION JUMBOTRON ########################## -->
+    
     <div class="container">
-      <div  class="jumbotron" border-color: #110022; >
-        <u><h1 class = "Display-1">Work Experience</h1></u>
-        <div id = "jumbotron3">
-          <br>
+      <div class="jumbotron" border-color: #110022; >
+        <u><h1>Education Time-line</h1></u>
+        <br>
         
-          <div class="td">
-            <div class="tdPicc">
-              <img alt = "TD logo" id = "tdPic"style="float:right"; src="td.png" width = "110" height = "110">
-            </div>
-            
-            <u><h3>Insider Risk Analyst - TD Canada Trust</h3></u>
-            <p>(April 2021 - August 2021)</p>
-            <h6>Work-Term Evaluation: Outstanding</h6>
+  <div class="legend3">
+    <ul class="legend">
+      <li><span class="superawesome"></span> Completed</li>
+      <li><span class="awesome"></span> In-Progress</li>
+      <li><span class="kindaawesome"></span> Anticipated</li>
+  </ul>
+  </div>
+  <br>
+  <hr>   
 
-              <blockquote class = "quote" cite="http://">
-                <p>"Amjad exceeded all performance expectations in respect to output, quality standards, delivery of goals and assignments." - TD Manager</p>
-                </blockquote>
+<br>
 
+
+<div float = left class="left">
+  <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+    <div class="card-header">2013 - 2017</div>
+    <div class="card-body">
+      <h5 class="card-title"><b>Rick Hansen Secondary School</b></h5>
+      <ul class = "cart-text">
+        <li>85% Average</li>
+        <li>Graduated with Honours</li>
+      </ul>
+    </div>
+  </div>
+</div>
+        
   
 
-            <hr>
-          </div>
-          
-          <div class="government">
-            <div class="govPicc">
-              <img alt = "Gov. logo" id = "govPic" style="float:right" src="gov.png" width = "150" height = "40">
-            </div>
-            
-            <u><h3>Business Analyst - Ontario Government</h3></u>
-            <p>(January 2021 - April 2021)</p>
-            <h6>Work-Term Evaluation: Excellent</h6>
-            <blockquote class = "quote" cite="http://">
-              <p>"Amjad was able to stay on task for multiple work streams and deliver on target dates." - Gov. Manager</p>
-            </blockquote>
-            <hr>
-          </div>
-        <div class="cgi">
-          <div class="cgiPicc">
-            <img alt = "CGI logo" id = "cgiPic"style="float:right"; src="GIB.png" width = "110" height = "110">
-          </div>
-          
-          <u><h3>Project Control Officer (PCO) - CGI</h3></u>
-          <p>(April 2019 – January 2020)</p>
-          <h6>Work-Term Evaluation: Outstanding</h6>
-          <blockquote id = "cgiQuote" cite="http://">
-            <p>"Amjad demonstrated he can be relied on in any situation. He asks for more responsiblity and he delivers." - CGI Director</p>
-          </blockquote>
-        </div>
-        <hr>
-        </div>
 
-        <!-- Downloads Resume -->
-          <button id ="workExpButton" type="submit"onclick="window.open('Amjad_Alsabagh_Resume.docx')" class="btn bg-info btn-outline-dark btn-lg">Download CV!</button>
+<div class="right">
+  <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
+    <div class="card-header">2017 - 2022</div>
+    <div class="card-body">
+      <h5 class="card-title">Ryerson University</h5>
+      <ul class = "cart-text">
+        <li>Business Technology Management Co-op</li>
+        <li>3.6 CGPA</li>
+        <li>Dean's List 2020, 2021</li>
+      </ul>
+      
+      <img alt = "A picture of Ryerson's Logo" src="rye.png" height = 150 width = 150 alt="">
+    </div>
+  </div>
+</div>
+
+<div class="left">
+  <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
+    <div class="card-header"> 2021 - 2022</div>
+    <div class="card-body">
+      <h5 class="card-title">Chang School of Continuing Education</h5>
+      <ul class = "cart-text">
+        <li>CCPS109 - Python</li>
+        <li>CCPS209 - Java OOP</li>
+        <li>CCPS305 - Data Structures</li>
+        <li>CCPS393 - C, C++ and UNIX</li>
+        <li>CCPS409 - Software Engineering</li>
+        
+      </ul>
+      <img alt = "A picture of the C & C++ Proramming Languages" float = "center" src="c-logo.png" height = 150 width = 150 alt="">
+    </div>
+    
+
+    
+  </div>
+</div>
+
+<div class="right">
+  <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+    <div class="card-header">2022 - 2023</div>
+    <div class="card-body">
+      <h5 class="card-title">Oracle Java SE Programmer</h5>
+      <ul class = "cart-text">
+        <li>Currently completing training materials</li>
+        <li>Certification exam - July 2022</li>
+        
+        
+      </ul>
+      <img alt = "A picture of the Java Proramming Language" float = "center" src="java.jpg" height = 150 width = 150 alt="">
+    </div>
+  </div>
+</div>
       </div>
     </div>
-  <!-- ####################### END WORK EXPERIENCE JUMBOTRON ########################## -->
-    <br><br><br><br>    
-
+    
+    
+    
+    <!-- ####################### END EDUCATION JUMBOTRON ########################## -->
   
-  
+  <br><br><br>
   <footer> <small>&copy;2021 Amjad Alsabagh</small> </footer> 
   <br>
+  
+  
+    
+
+
+    
   </body>
 </html> 
